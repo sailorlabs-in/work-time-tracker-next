493b0a8f8e6c039592d5b07d698fe484a65fee24
Umang Sailor <umang@weetechsolution.com>
2026-07-14 14:55:30 +0530

    Revert "ci: add lint stage and update eslint rules to warn level"
    This reverts commit c191bad81165a6911bbd72bad6ae61511d0d2f11.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,6 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-      "react-hooks/set-state-in-effect": "warn",
-    },
-  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -18,7 +12,6 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
-    "scripts/**",
   ]),
 ]);
 
